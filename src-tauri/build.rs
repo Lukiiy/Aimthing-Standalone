2e5871b6e58070d0cb349772bc7b56c1d8058e6f
Lukiiy <lukiiy@proton.me>
2026-07-15 17:08:41 -0300

    Fixed standalone script path and some minor changes

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,15 +1,15 @@
 use std::fs;
+use regex::Regex;
 
 fn main() {
     let html_path = "../src/index.html";
     let mut html = fs::read_to_string(html_path).expect("Failed to read main page");
 
-    html = regex::Regex::new(r"(<h5[^>]*>Version\s+[\d.]+)(</h5>)") // Patch edition
-        .unwrap()
+    html = Regex::new(r"(<h5[^>]*>Version\s+[\d.]+)(</h5>)").unwrap() // Patch edition
         .replace(&html, "$1 (Standalone)$2")
         .to_string();
 
-    html = html.replace("</head>", "  <script src=\"standalone.js\"></script>\n</head>"); // Patchin in standalone code
+    html = html.replace("</head>", "    <script src=\"scripts/standalone.js\"></script>\n</head>"); // Patchin in standalone code
 
     fs::write(html_path, html).expect("Failed to write main page");
 
